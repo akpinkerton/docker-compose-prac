e3e496a6697c26c8be7f5d67f12e957aa86211e3
akpinkerton <akpinkerton@gmail.com>
2021-07-14 12:01:30 -0700

    db doesnt display

---
+++ frontend/src/App.js
@@ -0,0 +1,48 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [data, setData] = useState({});
+  const [change, setChange] = useState('')
+
+  const handleChange = (e) => {
+    setChange(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    setData({title: change})
+    console.log("Change: ", change);
+    console.log("Data: ", data)
+  }
+
+  function postInputs() {
+    fetch('http://localhost:3001/', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+  }
+
+  useEffect(() => {
+    if (data.title !== '') {
+      postInputs()
+    }
+  }, [data])
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="TYPE SOMETHING HERE"
+        value={change}
+        onChange={handleChange}
+      ></input>
+      <button type="submit" value="submit" onClick={handleSubmit}>Click me</button>
+    </div>
+  );
+}
+
+export default App;
